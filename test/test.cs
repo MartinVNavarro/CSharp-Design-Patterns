657f29da37de91d93e7336e57e6c2944edbc515e
MartinVNavarro <martinvnavarro@gmail.com>
2018-08-20 14:33:37 -0700

    Added CoC, License and structure from VSCode

---
+++ test/test.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace csharp_design_patterns.test
+{
+    [TestFixture]
+    public class test
+    {
+        [Test]
+        public void Add()
+        {
+            
+        }
+    }
+}
