e0b2eefcdbfcb3657636439d0d675111d763f81a
unknown <martinvnavarro@gmail.com>
2018-08-31 23:14:02 -0700

    Completed memento classes/tests and README

--- DesignPatterns/Behavioral/Memento/Caretaker.cs
+++ DesignPatterns/Behavioral/Memento/Caretaker.cs
@@ -2,6 +2,9 @@ namespace DesignPatterns.Behavioral.Memento
 {
     public class Caretaker
     {
-        
+        /// <summary>
+        /// The saved memento that contains the saved state of the originator.
+        /// </summary>
+        public Memento Memento { get; set; }
     }
 }
--- DesignPatterns/Behavioral/Memento/Memento.cs
+++ DesignPatterns/Behavioral/Memento/Memento.cs
@@ -2,6 +2,9 @@ namespace DesignPatterns.Behavioral.Memento
 {
     public class Memento
     {
-        
+        /// <summary>
+        /// The saved state of an originator.
+        /// </summary>
+        public Originator.State SavedState { get; set; }
     }
 }
